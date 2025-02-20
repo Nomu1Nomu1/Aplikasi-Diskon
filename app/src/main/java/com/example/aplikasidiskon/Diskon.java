fbf52e0d3605aa7efef50c8e212ee7d08b7ebb47
Nomu1Nomu1 <isaiah.tino@gmail.com>
2025-02-21 01:17:41 +0700

    Final Diskon

--- app/src/main/java/com/example/aplikasidiskon/Diskon.java
+++ app/src/main/java/com/example/aplikasidiskon/Diskon.java
@@ -3,6 +3,13 @@ package com.example.aplikasidiskon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Diskon extends AppCompatActivity {
 
@@ -10,5 +17,38 @@ public class Diskon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diskon);
+
+        EditText etHarga = findViewById(R.id.nominal);
+        EditText etDiskon = findViewById(R.id.diskon);
+        TextView tvHasil = findViewById(R.id.hargaDiskon);
+        Button diskonButton = findViewById(R.id.diskonButton);
+
+        diskonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nominalStr = etHarga.getText().toString();
+                String diskonStr = etDiskon.getText().toString();
+
+                double nominal = Double.parseDouble(nominalStr);
+                double diskon = Double.parseDouble(diskonStr);
+
+                if (nominalStr.isEmpty() || diskonStr.isEmpty()) {
+                    tvHasil.setText("Please fill the blank fields");
+                    return;
+                }
+
+                if (diskon < 0 || diskon > 100) {
+                    tvHasil.setText("Diskon must be between 0 and 100");
+                    return;
+                }
+
+                double hargaSetelahDiskon = nominal - (nominal * diskon / 100);
+
+                NumberFormat formatter = NumberFormat.getNumberInstance(new Locale("id", "ID"));
+                String price = formatter.format(hargaSetelahDiskon);
+
+                tvHasil.setText("Rp "+price);
+            }
+        });
     }
 }
